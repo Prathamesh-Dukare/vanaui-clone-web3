e685e503edae8077973ecb8810f785d8308513f0
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-06-06 12:55:46 +0530

    Add : Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-wrapper">
+      <header>
+        <h1 class="text-3xl font-bold underline text-red-400">
+          Hello world!
+        </h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
